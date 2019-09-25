9869c50a0124147e2032fb55410d1c7c20b34906
ClassCVUSA <53262741+ClassCVUSA@users.noreply.github.com>
2019-09-25 08:00:32 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <stdio.h>
+
+#include "opencv2/core/core.hpp"
+#include "opencv2/core/utility.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/highgui/highgui_c.h"
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/imgproc.hpp"
+
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char** argv )
+{
+
+    Mat image;
+    image = imread( "lena.jpg",IMREAD_ANYCOLOR );
+
+    if ( !image.data )
+    {
+        printf("No image data \n");
+        return -1;
+    }
+    namedWindow("Display Image", CV_WINDOW_AUTOSIZE );
+    imshow("Display Image", image);
+
+    waitKey(0);
+
+    return 0;
+}
